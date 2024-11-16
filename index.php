61b0fbd63b2857a6586512744a8af91dd8571b6c
Theocharis Apostolidis <barbarangelopoulou@gmail.com>
2024-11-17 00:35:49 +0200

    update

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ require 'dbconnect.php';
 $conn = createConnection($servername, $username, $password, $database);
 
 if ($result = $conn -> query("SELECT * FROM board")) {
-    echo "Returned rows are: " . $stilistili;
+    echo "Returned rows are: " . $stili;
     // Free result set
     $result -> free_result();
   }
