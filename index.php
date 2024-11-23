a186ce9c614e4dcc1b95bc8597c96d025b0f341e
Theocharis Apostolidis <barbarangelopoulou@gmail.com>
2024-11-23 12:21:16 +0200

    update

--- index.php
+++ index.php
@@ -6,14 +6,18 @@ require 'dbconnect.php';
 $conn = createConnection($servername, $username, $password, $database);
 
 if ($result = $conn -> query("SELECT * FROM board")) {
-    echo  $result->fetch_assoc();
-    // Free result set
-    $result -> free_result();
+    if ($result->num_rows > 0) {
+      
+      // output data of each row
+      while($row = $result->fetch_assoc()) {
+        echo $row["stili"];
+      }
+      
   }
   
 
 header("HTTP/1.1 500 Internal Server Error");
 header('Content-Type: application/json;');
-echo '{"Response":"Internal Server Error", "StatusCode":500}';
+echo '{"Response":"Internal Server Error", "StatusCode":500r}';
 
 ?>
