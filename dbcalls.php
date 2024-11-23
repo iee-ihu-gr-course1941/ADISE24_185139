d71bbcd3774e54231a1487b82c2cf1c372936313
Theocharis Apostolidis <barbarangelopoulou@gmail.com>
2024-11-23 13:31:31 +0200

    created dbcalls file

---
+++ dbcalls.php
@@ -0,0 +1,20 @@
+<?php
+
+function get_board($conn) {
+  if ($result = $conn -> query("SELECT * FROM board")) {
+    if ($result->num_rows > 0) {
+      // output data of each row
+      $json = '{'; 
+      while($row = $result->fetch_assoc()) {
+        $json = $json . '"a' . $row["stili"] . '":"' . $row["a"] . '", "b' . $row["stili"] . '":"' . $row["b"] . '", "c' . $row["stili"] . '":"' . $row["c"] . '", "d' . $row["stili"] . '":"' . $row["d"] . '","e' . $row["stili"] . '":"' . $row["e"] . '", "f' . $row["stili"] . '":"' . $row["f"] . '", "g' . $row["stili"] . '":"' . $row["g"] . '",';
+      }
+      $json = substr_replace($json, '', -1);
+      $json = $json . '}';
+      header("HTTP/1.1 200 Ok");
+      header('Content-Type: application/json;');
+      echo $json;
+    }
+  }
+}
+
+?>
--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 require_once 'secrets.php';
 require 'dbconnect.php';
+require_once 'dbcalls.php';
 
 $request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
 
@@ -17,64 +18,6 @@ switch ($request[0]) {
     header('Content-Type: application/json;');
     echo '{"Response":"Endpoint not found", "StatusCode":404}';
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function get_board($conn) {
-  if ($result = $conn -> query("SELECT * FROM board")) {
-    if ($result->num_rows > 0) {
-      // output data of each row
-      $json = '{'; 
-      while($row = $result->fetch_assoc()) {
-        $json = $json . '"a' . $row["stili"] . '":"' . $row["a"] . '", "b' . $row["stili"] . '":"' . $row["b"] . '", "c' . $row["stili"] . '":"' . $row["c"] . '", "d' . $row["stili"] . '":"' . $row["d"] . '","e' . $row["stili"] . '":"' . $row["e"] . '", "f' . $row["stili"] . '":"' . $row["f"] . '", "g' . $row["stili"] . '":"' . $row["g"] . '",';
-      }
-      $json = substr_replace($json, '', -1);
-      $json = $json . '}';
-      header("HTTP/1.1 200 Ok");
-      header('Content-Type: application/json;');
-      echo $json;
-    }
-  }
-}
-  
-
 
 
 ?>
