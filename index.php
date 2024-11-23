1efd904809b7694c8ae86a836d8a1589c6b13515
Theocharis Apostolidis <barbarangelopoulou@gmail.com>
2024-11-23 13:07:16 +0200

    print board

--- index.php
+++ index.php
@@ -8,9 +8,12 @@ $conn = createConnection($servername, $username, $password, $database);
 if ($result = $conn -> query("SELECT * FROM board")) {
     if ($result->num_rows > 0) {
       // output data of each row
+      $json = '{'; 
       while($row = $result->fetch_assoc()) {
-        echo '{"a' . $row["stili"] . '":"' . $row["a"] . '", "b' . $row["stili"] . '":"' . $row["b"] . '", "c' . $row["stili"] . '":"' . $row["c"] . '", "d' . $row["stili"] . '":"' . $row["d"] . '","e' . $row["stili"] . '":"' . $row["e"] . '", "f' . $row["stili"] . '":"' . $row["f"] . '", "g' . $row["stili"] . '":"' . $row["g"] . '"}';
+        $json = $json . '"a' . $row["stili"] . '":"' . $row["a"] . '", "b' . $row["stili"] . '":"' . $row["b"] . '", "c' . $row["stili"] . '":"' . $row["c"] . '", "d' . $row["stili"] . '":"' . $row["d"] . '","e' . $row["stili"] . '":"' . $row["e"] . '", "f' . $row["stili"] . '":"' . $row["f"] . '", "g' . $row["stili"] . '":"' . $row["g"] . '",';
       }
+      $json = substr_replace($json, '', -1);
+      $json = $json . '}';
   }
 }
   
