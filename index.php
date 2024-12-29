13435050ca6f66150bd0f4e75140cde7d72e80fb
Theocharis Apostolidis <barbarangelopoulou@gmail.com>
2024-12-29 22:41:23 +0200

    Added func to extract Bearer Token

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
 
 $conn = createConnection($servername, $username, $password, $database);
 
-$currentPlayer = $_SERVER['Authorization'];
+$currentPlayer = getBearerToken();
 
 switch ($request[0]) {
   case "board":
@@ -32,6 +32,40 @@ switch ($request[0]) {
     echo '{"Response":"Endpoint not found", "StatusCode":404}';
 }
 
+
+// Found on StackOverflow: https://stackoverflow.com/questions/40582161/how-to-properly-use-bearer-tokens
+function getAuthorizationHeader(){
+  $headers = null;
+  if (isset($_SERVER['Authorization'])) {
+      $headers = trim($_SERVER["Authorization"]);
+  }
+  else if (isset($_SERVER['HTTP_AUTHORIZATION'])) { //Nginx or fast CGI
+      $headers = trim($_SERVER["HTTP_AUTHORIZATION"]);
+  } elseif (function_exists('apache_request_headers')) {
+      $requestHeaders = apache_request_headers();
+      // Server-side fix for bug in old Android versions (a nice side-effect of this fix means we don't care about capitalization for Authorization)
+      $requestHeaders = array_combine(array_map('ucwords', array_keys($requestHeaders)), array_values($requestHeaders));
+      //print_r($requestHeaders);
+      if (isset($requestHeaders['Authorization'])) {
+          $headers = trim($requestHeaders['Authorization']);
+      }
+  }
+  return $headers;
+}
+
+/**
+* get access token from header
+* */
+function getBearerToken() {
+  $headers = getAuthorizationHeader();
+  // HEADER: Get the access token from the header
+  if (!empty($headers)) {
+      if (preg_match('/Bearer\s(\S+)/', $headers, $matches)) {
+          return $matches[1];
+      }
+  }
+  return null;
+}
 
 ?>
 
